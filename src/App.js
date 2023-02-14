df8fef67afd9946b5f8341bc2a64143a9c5ef886
Anosh Malik <anoshmalik.am@gmail.com>
2023-02-14 15:26:56 +0000

    Added title and header.jsx file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './css/App.css';
 import Names from './components/Names';
 import Search from './components/Search';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Search/>
       <Names/>
       
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div style={{ paddingBottom: "10px", paddingTop: "10px" }}>
+      <h1>Baby Name Picker App</h1>
+    </div>
+  );
+}
+
+export default Header
