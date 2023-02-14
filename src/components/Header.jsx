ea434dd13e460608e413eff458fcb85fc8789d77
Anosh Malik <anoshmalik.am@gmail.com>
2023-02-14 16:14:04 +0000

    Aligned image and searchbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
     <div style={{ paddingBottom: "10px", paddingTop: "10px" }}>
-      <h1>Baby Name Picker App</h1>
+      <img
+        style={{ width: "5em", height: "5em", paddingBottom: "10px" }}
+        src="https://www.svgrepo.com/show/224114/feeder.svg"
+        alt="bottle"
+      />
+      <h1>Baby Name Picker</h1>
     </div>
   );
 }
 
-export default Header
+export default Header;
