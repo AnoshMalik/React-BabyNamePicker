5f53c3d60b8b2a75c094d95451a5155dbacdfd21
Anosh Malik <anoshmalik.am@gmail.com>
2023-02-14 11:36:18 +0000

    Added tailwind to package.json

--- src/components/Names.jsx
+++ src/components/Names.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import data from "../data/babyNamesData.json";
 
 const Names = () => {
-    const dataType = data.sort(function (a, b) {
+    const names = data.sort(function (a, b) {
         if (a.name < b.name) {
             return -1;
         } else { 
@@ -10,14 +10,14 @@ const Names = () => {
         }
     });
 
-  console.log(dataType);
+//   console.log(names);
   return (
     <div>
-      {dataType.map(item => {
-        if (item.sex == "m") {
-          return <button className="btn btn-primary">{item.name}</button>;
+      {names.map((item,key) => {
+        if (item.sex === "m") {
+            return <button key={key} className="btn btn-primary">{item.name}</button>;
         } else {
-          return <button className="btn btn-success">{item.name}</button>;
+            return <button key={key} className="btn btn-success">{item.name}</button>;
         }
       })}
     </div>
